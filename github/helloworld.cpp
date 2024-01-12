7f880b21b9f1f8a6917e497988f0dd7f3353d31c
harshrakholiya12 <harshrakholiya90@gmail.com>
2024-01-12 14:37:39 +0530

    12-1-2024

---
+++ github/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "my name is harsh"<<std::endl;
+    std::cout << "my fathar name is ratibhai"<< std::endl;
+   std::cout << "my sister name is dency"<<std::endl;
+    return 0; 
+}
